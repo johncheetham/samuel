0a0871837310e51e68b87411979984d7f723d1b7
Thierry <thierry@substantiel.fr>
2019-08-27 12:21:33 +0200

    Generation et Clean make by setup.py.

--- create_po.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Make curlew.pot file from python source files.
-mkdir -p locale
-make -C po update-po
-make -C po
-make -C po install
-rm -f po/*.mo
--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from distutils.core import setup, Extension
+from distutils.command.clean import clean
 from glob import glob
 import os
 
@@ -10,16 +11,30 @@ module1 = Extension('samuel.engine',
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-def create_mo_files():
+def create_localised_files():
     mo_files = []
-    os.system('bash create_po.sh')
+    # os.system('bash create_po.sh')
+    os.system('make -C po')
+    os.system('make -C po DESTDIR=../ install')
     mo_files.append(('share/locale/de/LC_MESSAGES/', ['locale/de/LC_MESSAGES/samuel.mo']))
     mo_files.append(('share/locale/en/LC_MESSAGES/', ['locale/en/LC_MESSAGES/samuel.mo']))
     mo_files.append(('share/locale/es/LC_MESSAGES/', ['locale/es/LC_MESSAGES/samuel.mo']))
     mo_files.append(('share/locale/fr/LC_MESSAGES/', ['locale/fr/LC_MESSAGES/samuel.mo']))
     mo_files.append(('share/locale/it/LC_MESSAGES/', ['locale/it/LC_MESSAGES/samuel.mo']))
     mo_files.append(('share/locale/nl/LC_MESSAGES/', ['locale/nl/LC_MESSAGES/samuel.mo']))
+    mo_files.append(('share/applications',['desktop/samuel.desktop']))
     return mo_files
+
+class CleanFiles(clean):
+    def run(self):
+        super().run()
+        cmd_list = dict(
+            po_clean='make -C po clean',
+            irm_locale_and_desktop='rm -rf locale samuel.desktop'
+        )
+        for key, cmd in cmd_list.items():
+            os.system(cmd)
+
 
 setup (name = 'samuel',
     version = '0.1.9',
@@ -33,7 +48,11 @@ setup (name = 'samuel',
 
     license = "GPLv3+",
 
-    packages=['samuel'],    
+    packages=['samuel'],
+
+    cmdclass={
+        'clean': CleanFiles,
+    },
 
     classifiers=[
           'Development Status :: 4 - Beta',
@@ -48,9 +67,8 @@ setup (name = 'samuel',
     data_files = [("share/samuel/images", glob('images/*.png')),
             ("share/samuel/data", ['data/2pc.cdb', 'data/3pc.cdb', 'data/4pc.cdb', 'data/opening.gbk']),
             ("share/doc/samuel-0.1.9", ["README.rst", "LICENSE"]),
-            ('share/applications',['samuel.desktop']),
             ('share/pixmaps', ['samuel.png']),
-    ]+ create_mo_files(),    
+    ]+ create_localised_files(),
     scripts = [
         'scripts/samuel'
     ]
